030617531594967f4f7ea7536fe145040f007f83
Blake Bouchard <thatsblake@gmail.com>
2014-01-22 22:01:54 -0700

    Bunch of Stuff

---
+++ Startender/Assets/Scripts/BubbleButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleButton : MonoBehaviour {
+
+    // SHE'S SO FAAAAT
+    // BUBUBUBUBUBUBUBUBBUBUBBLE-BUTTON
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

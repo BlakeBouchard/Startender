fb910f92ed7f32c971cf94dfc75149743ff900e5
Blake Bouchard <thatsblake@gmail.com>
2014-01-30 15:32:05 -0700

    Title Screen

---
+++ Startender/Assets/Scripts/NextScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}

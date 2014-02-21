44f1914ba7a285b071a11b7b47d9ca7180298eb8
Blake Bouchard <thatsblake@gmail.com>
2014-02-21 11:37:33 -0700

    Basic Slider Functionality
    * Started new test scene for slider
    * Prefab for slider
    * Slider drags out on the X coordinate by default

---
+++ Startender/Assets/Scripts/SliderDrag.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderDrag : MonoBehaviour {
+
+    private Vector3 startPosition;
+    public bool rightToLeft = true;
+    
+    // Set this if we want the slider to snap back to its original position on release
+    public bool snapsBack = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPosition = transform.position;
+	}
+
+    // This function should send the slider back to its starting position 
+    void SnapBack ()
+    {
+        transform.position = startPosition;
+    }
+
+    // This code should handle moving the slider from left to right
+    void MoveSliderHorizontally (Vector3 newPosition)
+    {
+        if ((rightToLeft && newPosition.x < startPosition.x) || (!rightToLeft && newPosition.x > startPosition.x))
+        {
+            transform.position = new Vector3(newPosition.x, startPosition.y, startPosition.z);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    // Called when user touches a collider attached to this object
+    void OnTouchDrag (Touch touch)
+    {
+        MoveSliderHorizontally(Camera.main.ScreenToWorldPoint(touch.position));
+    }
+
+    // Called when user lifts their finger from a collider attached to this object
+    void OnTouchUp (Touch touch)
+    {
+        OnMouseUp();
+    }
+
+    // Called when user clicks mouse on a collider attached to this object
+    void OnMouseDown ()
+    {
+        Debug.Log("Clicked slider button");
+    }
+
+    // Called when user clicks a collider attached to this object
+    void OnMouseDrag ()
+    {
+        MoveSliderHorizontally(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    // Called when user releases the mouse button while dragging a collider attached to this object
+    void OnMouseUp ()
+    {
+        if (snapsBack)
+        {
+            SnapBack();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}

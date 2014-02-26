055fd8940c42c0a45bb4f974b60e28d0ce3ce480
Blake Bouchard <thatsblake@gmail.com>
2014-02-26 15:58:28 -0700

    Created a new scene that instantiates prefabs
    * New scene now generates all important objects from prefabs rather than
    having them placed in the scene
    * This should save a bit of headache for saving and reloading mainscene

---
+++ Startender/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    public Transform cannonPrefab;
+    public Transform cupPrefab;
+    public Transform trayPrefab;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        InstantiateCannon();
+        InstantiateCup();
+        InstantiateTray();
+	}
+
+    private void InstantiateCannon()
+    {
+        Instantiate(cannonPrefab);
+    }
+
+    private void InstantiateCup()
+    {
+        Instantiate(cupPrefab);
+    }
+
+    private void InstantiateTray()
+    {
+        Instantiate(trayPrefab);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}

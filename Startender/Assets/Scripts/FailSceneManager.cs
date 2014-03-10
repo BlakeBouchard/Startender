6bde8a307d41e9ebf920ea51738b25311dece475
derekdowling <thederekdowling@gmail.com>
2014-03-10 11:57:48 -0600

    Missing files

---
+++ Startender/Assets/Scripts/FailSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FailSceneManager : MonoBehaviour {
+
+	private PlayerState player;
+
+	public int menuXFromCenter;
+	public int menuYFromCenter;
+	public int menuWidth;
+	public int menuHeight;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindObjectOfType<PlayerState>();
+
+		//setup basic drawing params
+		this.menuWidth = 240;
+		this.menuHeight = 200;
+		this.menuXFromCenter = this.menuWidth / 2;
+		this.menuYFromCenter = this.menuHeight / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		
+		GUILayout.BeginArea(new Rect(Screen.width / 2 - this.menuXFromCenter, Screen.height /2 - this.menuYFromCenter, this.menuWidth, this.menuHeight));
+
+		GUILayout.Label(player.GetFailMessage());
+		if(GUILayout.Button("Back To Main Menu")) {
+			Debug.Log("Loading main menu");
+			Application.LoadLevel("titleScreen");
+		}
+
+		GUILayout.EndArea();
+	}
+}

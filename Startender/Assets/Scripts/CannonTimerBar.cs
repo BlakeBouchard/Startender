7db6f6b7ae7575051239d78808783653b8858608
Blake Bouchard <thatsblake@gmail.com>
2014-04-05 08:05:43 -0600

    Fixed bubbles not colliding with cup
    * Also cannon timer bar doesn't spam debug log any more

--- Startender/Assets/Scripts/CannonTimerBar.cs
+++ Startender/Assets/Scripts/CannonTimerBar.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class CannonTimerBar : MonoBehaviour {
 
-    float maxScale = 0.2620136f;
+    public float maxScale = 0.2620136f;
 
 	// Set the length of the bar.
 	void SetLength (float percent) {
 
 	    float length = percent * maxScale;
-        Debug.Log("Percent: " + percent + ", Length: " + length);
+        // Debug.Log("Percent: " + percent + ", Length: " + length);
         gameObject.transform.localScale = new Vector3(length, maxScale, 0);
 	}
 }

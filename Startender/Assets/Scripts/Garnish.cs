be63cf137987c59b9f7b4da4e455f59533ee6908
Blake Bouchard <thatsblake@gmail.com>
2014-04-06 12:28:04 -0600

    Testing for duplicate ingredients
    * Duplicate ingredients work with the Drink population script
    * Since everything gets destroyed at the end of round anyway when the
    scene changes over, the Bubble and Garnish scripts don't need to contain
    any references to gameManager

--- Startender/Assets/Scripts/Garnish.cs
+++ Startender/Assets/Scripts/Garnish.cs
@@ -3,18 +3,12 @@ using System.Collections;
 
 public class Garnish : MonoBehaviour
 {
-    GameObject gameManager;
-	GameManager gameManagerScript;
-
     public Transform ingredientPrefab;
     private Ingredient attachedIngredient;
 
 	// Use this for initialization
 	void Start ()
 	{
-		gameManager = GameObject.Find("Game Manager");
-		gameManagerScript = (GameManager) gameManager.GetComponent(typeof(GameManager));
-
         Transform ingredientObject = Instantiate(ingredientPrefab) as Transform;
         ingredientObject.name = ingredientPrefab.name;
         ingredientObject.parent = this.transform;
@@ -26,10 +20,7 @@ public class Garnish : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (gameManagerScript.GetGameState () == GameManager.GameState.RoundOver) {
-			Debug.Log("Destroying garnish at end of round");
-			this.Die();
-		}
+		
 	}
 
 	public Ingredient GetIngredient() 
